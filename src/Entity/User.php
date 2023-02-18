cece08e3d0cc82f6bb38577d982baf9859a35840
Thodler <neoskywalker01@gmail.com>
2023-02-18 21:58:27 +0100

    [UPD] User.php.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -257,4 +257,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->pseudo;
+    }
 }
