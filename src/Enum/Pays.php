6ee850445401ef4e133d1ff8bf027c126c154c06
Thodler <neoskywalker01@gmail.com>
2023-02-18 18:28:15 +0100

    [ADD] Enum des Pays.

---
+++ src/Enum/Pays.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enum;
+
+enum Pays: string
+{
+    case ALLEMAGNE = 'allemagne';
+    case AMERIQUE = 'amerique';
+    case ANGLETERRE = 'angleterre';
+    case BRESIL = 'bresil';
+    case CANADA = 'canada';
+    case JAPON = 'japon';
+    case FRANCE = 'france';
+}
